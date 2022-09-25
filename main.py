402e540f3469ec011652cdeb9ac63997bdef7ed1
Leonardo Almeida <leonardoalmd8@outlook.com>
2022-09-25 03:08:39 +0000

    demo for git commit and push

---
+++ main.py
@@ -0,0 +1,15 @@
+lista = []
+
+def lista1():
+    while True:
+        inserir = input('Digite oq deseja inserir: ')
+        lista.append(inserir)
+
+        for i in range(0, len(lista)):
+            print(lista[i])
+        
+        if inserir == 'sair':
+            break
+
+
+lista1()
