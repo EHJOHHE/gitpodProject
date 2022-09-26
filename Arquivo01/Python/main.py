1118c966d113d119c660891e9d87c9ad0c4a7c3a
LeonardoAlmd <leonardoalmd8@outlook.com>
2022-09-25 23:57:03 -0300

    arrumando a pasta python

--- Arquivo01/Python/main.py
+++ Arquivo01/Python/main.py
@@ -1,3 +1,76 @@
-import functions
+def listaF():
+    lista = list()
+    while True:
+        inserir = input('\nDigite oq deseja inserir: ')
+        if inserir != 'sair':
+            lista.append(inserir)
 
-functions.dicionarioF()
+        '''for i in lista:
+            print(i)'''
+
+        print('-=' * 30)
+
+        for i in range(len(lista)):
+            print(f'{i + 1} - {lista[i].upper()}')
+
+        if inserir == 'sair':
+            break
+
+
+def dicionarioF():
+    pessoasCadastradas = list()
+    pessoa = dict()
+    soma = 0
+
+    while True:
+        pessoa.clear()
+        print('-=' * 30)
+        pessoa['nome'] = input('Nome: ').upper()
+
+        while True:
+            pessoa['sexo'] = input('Sexo: [M/F/NB]: ').upper()[0]
+            if pessoa['sexo'] in 'MFNB':
+                break
+            print('ERRO! Por favor digite [M] ou [F].')
+
+        pessoa['idade'] = int(input('Idade: '))
+        soma += pessoa['idade']
+        pessoasCadastradas.append(pessoa.copy())
+
+        while True:
+            option = input('Deseja Continuar? [S/N] ').upper()[0]
+            if option in 'SN':
+                break
+            print('ERRO! Por favor digite Sim ou Não.')
+
+        if option == 'N':
+            break
+
+    media = soma / len(pessoasCadastradas)
+
+    print('-=' * 30)
+    print(f'A) NO TOTAL FORAM CADASTRADAS {len(pessoasCadastradas)} PESSOAS.')
+    print(f'B) A MÉDIA DE IDADE É {media:5.2f}')
+    print('C) AS MULHERES CADASTRADAS SÃO: ', end='')
+    for p in pessoasCadastradas:
+        if p['sexo'] == 'F':
+            print(f'{p["nome"]}, ', end='')
+    print()
+
+    print('D) AS PESSOAS ACIMA DA MÉDIA SÃO: ', end='')
+    for p in pessoasCadastradas:
+        if p['idade'] >= media:
+            print(f'{p["nome"]}, ', end='')
+    print()
+
+    print('E) AS PESSOAS ABAIXO DA MÉDIA SÃO: ', end='')
+    for p in pessoasCadastradas:
+        if p['idade'] < media:
+            print(f'{p["nome"]}, ', end='')
+    print('\n\n')
+
+    print(f"{'<ENCERRADO>': ^50}")
+    print('-=' * 30)
+
+
+dicionarioF()
